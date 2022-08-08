f306e8b5d58e375d1c43eb84c192262c0cf077d2
flash-of-genius <amhdaar@gmail.com>
2022-08-08 11:56:36 +0100

    turn uncontrolled input to controlled one: 5 - Handling Form Submittal

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,13 +1,22 @@
 import React from "react";
 
 class SearchBar extends React.Component {
+  state = { term: "" };
+
+  onInputChange = (event) => {
+    this.setState({ term: event.target.value });
+  };
   render() {
     return (
       <div className="search-bar ui segment">
         <form action="" className="ui form">
           <div className="field">
             <label htmlFor="">Video Search</label>
-            <input type="text" />
+            <input
+              value={this.state.term}
+              onChange={this.onInputChange}
+              type="text"
+            />
           </div>
         </form>
       </div>
