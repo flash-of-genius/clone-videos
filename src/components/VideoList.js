fb451b6f1c0c88c274fc664d4b88b48ad850a66e
flash-of-genius <amhdaar@gmail.com>
2022-08-08 20:11:32 +0100

    map over videos array

--- src/components/VideoList.js
+++ src/components/VideoList.js
@@ -2,6 +2,9 @@ import React from "react";
 import VideoItem from "./VideoItem";
 
 const VideoList = ({ videos }) => {
+  const renderedList = videos.map((video) => {
+    return <VideoItem />;
+  });
   return <div>{videos.length}</div>;
 };
 
