10e69bed9f7f26583ee6f59c84a9a1cc9ce653fe
flash-of-genius <amhdaar@gmail.com>
2022-08-08 16:09:48 +0100

    setup axios

--- src/apis/youtube.js
+++ src/apis/youtube.js
@@ -1,1 +1,12 @@
+import axios from "axios";
+
 const KEY = "AIzaSyCmM6BmBgJzonLvWkwR9Cp7-aswQ5jYiKQ";
+
+export default axios.create({
+  baseURL: "https://www.googleapis.com/youtube/v3",
+  params: {
+    part: "snippet",
+    maxResults: 5,
+    key: KEY,
+  },
+});
